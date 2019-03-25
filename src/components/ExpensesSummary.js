238a3c8cda7656ff0639a00939ed8c23a54982a7
Dragan <dragan@jl.is>
2019-03-25 11:06:14 +0100

    Setup test database env.

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,15 +4,16 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  
   return (
     <div>
-      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal} </h1>
+      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
@@ -20,7 +21,7 @@ const mapStateToProps = (state) => {
   return {
     expenseCount: visibleExpenses.length,
     expensesTotal: selectExpensesTotal(visibleExpenses)
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(ExpensesSummary);
